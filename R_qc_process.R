7fd87fc3346f729711ba3f2a9cdd6a512cd2d1b0
Lironghu_Rose <112094462+lironghu@users.noreply.github.com>
2026-05-13 15:38:55 +0800

    Update R_qc_process.R

--- R_qc_process.R
+++ R_qc_process.R
@@ -119,4 +119,4 @@ all_merge <- merge(x = all_list[[1]],
 #save data
 names(all_merge@meta.data)
 table(all_merge@meta.data$orig.ident)
-saveRDS(all_merge, file ="chicken_hip_scRNA_qc_merged_3_sample8.rds")
+saveRDS(all_merge, file ="chicken_hip_scRNA_qc_merged_8_samples.rds")
